d6f75987bcc63f3fa01411b1e08e95e6fd0b5865
buddapallavi127 <buddapallavi057@gmail.com>
2024-11-02 15:45:31 +0530

    login page by pallavi

--- loginpage.cpp
+++ loginpage.cpp
@@ -1,8 +1,13 @@
-#include<iostream>
+#include <mysql_driver.h>
+#include <mysql_connection.h>
+#include <cppconn/prepared_statement.h>
+#include <cppconn/resultset.h>
+#include <iostream>
+#include <string>
+
 using namespace std;
 int main(){
     cout<<"Login_details";
     cout<<"edited by pallavi";
-    cout<<"check";
     return 0;
 }
